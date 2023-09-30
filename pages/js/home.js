9211d2b2034729456b1319c2ebd7c3aa2041ab0c
Luiz Cesar <luiizcesar.lc@gmail.com>
2023-09-30 16:26:55 -0300

    Adicionado campo de pesquisa, ainda em andamento

--- pages/js/home.js
+++ pages/js/home.js
@@ -1,4 +1,4 @@
-//Função para logout da pagina
+// Função para logout da página
 function logout() {
   firebase
     .auth()
@@ -9,4 +9,78 @@ function logout() {
     .catch(() => {
       alert('Erro ao fazer logout');
     });
+}
+
+// Campo de pesquisa
+let sugestoes = [
+  'carro',
+  'smartphone',
+  'iphone',
+  'samsung',
+  'xiaomi',
+  'videogame',
+  'jogo',
+  'celular',
+  'bicicleta',
+  'musica',
+  'cd',
+  'maquiagem',
+  'roupa',
+  'tenis',
+  'sandalia',
+  'camisa',
+  'casaco',
+];
+
+const searchWrapper = document.querySelector('.search');
+const inputBox = searchWrapper.querySelector('input');
+const sugestBox = searchWrapper.querySelector('.list');
+const buttonSearch = searchWrapper.querySelector('.searchButton');
+let linkTag = searchWrapper.querySelector('a');
+let webLink;
+
+inputBox.oninput = (e) => {
+  let userData = e.target.value;
+  let emptyArray = [];
+
+  if (e.key == 'Enter') {
+    if (userData) {
+      window.open(`https://www.google.com/search?q=${userData}`);
+    }
+  }
+
+  if (userData) {
+    buttonSearch.onclick = () => {
+      webLink = `https://www.google.com/search?q=${userData}`;
+      linkTag.setAttribute('href', webLink);
+      linkTag.click();
+    };
+    emptyArray = sugestoes.filter((data) => {
+      return data.toLowerCase().startsWith(userData.toLowerCase());
+    });
+    emptyArray = emptyArray.map((data) => {
+      return (data = `<li>${data}</li>`);
+    });
+    searchWrapper.classList.add('active');
+    showSuggestions(emptyArray);
+    let allList = sugestBox.querySelectorAll('li');
+    for (let i = 0; i < allList.length; i++) {
+      allList[i].setAttribute('onclick', 'select(this)');
+    }
+  } else {
+    searchWrapper.classList.remove('active');
+  }
+};
+
+// Função para mostrar sugestões
+function showSuggestions(list) {
+  let listData;
+  if (!list.length) {
+    userData = inputBox.value;
+    listData = `<li>${userData}</li>`;
+  } else {
+    listData = list.join('');
+  }
+
+  sugestBox.innerHTML = listData;
 }
